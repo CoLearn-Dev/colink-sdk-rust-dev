2cfb14fc3a9fdf86740d6cfbd364ec1e70fe51de
nociza <azicon@berkeley.edu>
2022-11-02 15:22:39 -0700

    All logic except read

--- src/application.rs
+++ src/application.rs
@@ -171,21 +171,20 @@ impl CoLink {
     ) -> Result<String, Error> {
         let mut client = self._grpc_connect(&self.core_addr).await?;
         let metadata_key = format!("{}::metadata", key_name);
+        let metadata_key_copy = metadata_key.clone();
+        let result = metadata_key.clone();
         let num_chunks = payload.len() / CHUNK_SIZE + 1;
 
-        let request = generate_request(
+        let create_request = generate_request(
             &self.jwt,
             StorageEntry {
                 key_name: metadata_key,
-                payload: num_chunks.to_string().into_bytes(), // simply store the number of chunks in the metadata
+                payload: (num_chunks.to_string() + "-locked").into_bytes(), // simply store the number of chunks in the metadata
                 ..Default::default()
             },
         );
 
-        let response = client.create_entry(request).await?;
-        if response.get_ref().status != 0 {
-            return Err(format!("create_entry failed: {:?}", response.get_ref().status).into());
-        }
+        client.create_entry(create_request).await?;
 
         let mut offset = 0;
         let mut chunk_id = 0;
@@ -205,16 +204,24 @@ impl CoLink {
                 },
             );
 
-            let response = client.create_entry(request).await?;
-            if response.get_ref().status != 0 {
-                return Err(format!("create_entry failed: {:?}", response.get_ref().status).into());
-            }
+            client.create_entry(request).await?;
 
             offset += chunk_size;
             chunk_id += 1;
         }
 
-        Ok(metadata_key.clone())
+        let update_request = generate_request(
+            &self.jwt,
+            StorageEntry {
+                key_name: metadata_key_copy,
+                payload: (num_chunks.to_string() + "-unlocked").into_bytes(),
+                ..Default::default()
+            },
+        );
+
+        client.update_entry(update_request).await?;
+
+        Ok(result)
     }
 
     pub async fn create_entry(&self, key_name: &str, payload: &[u8]) -> Result<String, Error> {
@@ -241,6 +248,35 @@ impl CoLink {
         debug!("RESPONSE={:?}", response);
         Ok(response.get_ref().key_path.clone())
     }
+
+    // pub async fn _read_entry_bulk(&self, key_name: &str) -> Result<Vec<u8>, Error> {
+    //     let mut client = self._grpc_connect(&self.core_addr).await?;
+    //     let metadata_key = format!("{}::metadata", key_name);
+    //     let request = generate_request(&self.jwt, StorageEntry {
+    //         key_name: metadata_key,
+    //         ..Default::default()
+    //     });
+    //
+    //     let response = client.read_entries(request).await?;
+    //
+    //     let num_chunks = std::str::from_utf8(&response.get_ref().payload).unwrap().parse::<usize>().unwrap();
+    //     let mut payload = Vec::new();
+    //     for chunk_id in 0..num_chunks {
+    //         let request = generate_request(&self.jwt, StorageEntry {
+    //             key_name: format!("{}::{}", key_name, chunk_id),
+    //             ..Default::default()
+    //         });
+    //
+    //         let response = client.get_entry(request).await?;
+    //         if response.get_ref().status != 0 {
+    //             return Err(format!("get_entry failed: {:?}", response.get_ref().status).into());
+    //         }
+    //
+    //         payload.extend_from_slice(&response.get_ref().payload);
+    //     }
+    //
+    //     Ok(payload)
+    // }
 
     pub async fn read_entries(&self, entries: &[StorageEntry]) -> Result<Vec<StorageEntry>, Error> {
         let mut client = self._grpc_connect(&self.core_addr).await?;
@@ -271,8 +307,79 @@ impl CoLink {
         Ok(res[0].payload.clone())
     }
 
+    pub async fn _update_entry_bulk(
+        &self,
+        key_name: &str,
+        payload: &[u8],
+    ) -> Result<String, Error> {
+        let mut client = self._grpc_connect(&self.core_addr).await?;
+        let metadata_key = format!("{}::metadata", key_name);
+        let metadata_key_copy = metadata_key.clone();
+        let result = metadata_key.clone();
+        let num_chunks = payload.len() / CHUNK_SIZE + 1;
+
+        let request = generate_request(
+            &self.jwt,
+            StorageEntry {
+                key_name: metadata_key,
+                // store number of chunks and lock in metadata
+                payload: (num_chunks.to_string() + "-locked").into_bytes(),
+                ..Default::default()
+            },
+        );
+
+        client.update_entry(request).await?;
+
+        let mut offset = 0;
+        let mut chunk_id = 0;
+        while offset < payload.len() {
+            let chunk_size = if offset + CHUNK_SIZE as usize > payload.len() {
+                payload.len() - offset
+            } else {
+                CHUNK_SIZE as usize
+            };
+            let chunk = payload[offset..offset + chunk_size].to_vec();
+            let request = generate_request(
+                &self.jwt,
+                StorageEntry {
+                    key_name: format!("{}::{}", key_name, chunk_id),
+                    payload: chunk,
+                    ..Default::default()
+                },
+            );
+
+            client.update_entry(request).await?;
+
+            offset += chunk_size;
+            chunk_id += 1;
+        }
+
+        let update_request = generate_request(
+            &self.jwt,
+            StorageEntry {
+                key_name: metadata_key_copy,
+                payload: (num_chunks.to_string() + "-unlocked").into_bytes(),
+                ..Default::default()
+            },
+        );
+
+        client.update_entry(update_request).await?;
+
+        Ok(result)
+    }
+
     pub async fn update_entry(&self, key_name: &str, payload: &[u8]) -> Result<String, Error> {
         let mut client = self._grpc_connect(&self.core_addr).await?;
+
+        if key_name.contains("$") {
+            let token = key_name.split("$").last().unwrap();
+            if token.eq("chunk") {
+                let response = self._update_entry_bulk(key_name, payload).await?;
+                return Ok(response);
+            }
+            return Err("invalid storage option".into());
+        }
+
         let request = generate_request(
             &self.jwt,
             StorageEntry {
@@ -288,6 +395,21 @@ impl CoLink {
 
     pub async fn delete_entry(&self, key_name: &str) -> Result<String, Error> {
         let mut client = self._grpc_connect(&self.core_addr).await?;
+
+        if key_name.contains("$") {
+            let token = key_name.split("$").last().unwrap();
+            if token.eq("chunk") {
+                let metadata_key = format!("{}::metadata", key_name);
+                let request = generate_request(&self.jwt, StorageEntry {
+                    key_name: metadata_key,
+                    ..Default::default()
+                });
+                let response = client.delete_entry(request).await?;
+                return Ok(response.get_ref().key_path.clone());
+            }
+            return Err("invalid storage option".into());
+        }
+
         let request = generate_request(
             &self.jwt,
             StorageEntry {
