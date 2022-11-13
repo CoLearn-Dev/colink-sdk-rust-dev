2c0550036a3e7218d7e031c980b0b3d92c589196
nociza <azicon@berkeley.edu>
2022-11-13 04:56:25 -0800

    ran clippy

--- src/extensions.rs
+++ src/extensions.rs
@@ -4,6 +4,8 @@ mod get_participant_id;
 pub mod instant_server;
 #[cfg(feature = "extensions")]
 mod lock;
+#[cfg(feature = "magic_mount")]
+mod magic_mount;
 #[cfg(feature = "policy_module")]
 pub mod policy_module;
 #[cfg(feature = "extensions")]
@@ -18,7 +20,5 @@ mod switch_to_generated_user;
 mod variable_transfer;
 #[cfg(feature = "extensions")]
 mod wait_task;
-#[cfg(feature = "magic_mount")]
-mod magic_mount;
 #[cfg(feature = "extensions")]
 mod wait_user_init;
