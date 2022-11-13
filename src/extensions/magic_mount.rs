e608445a63b47b3b6339fd9cae4043c1506270d4
nociza <azicon@berkeley.edu>
2022-11-13 07:24:42 -0800

    allow async recursion and fix cb

--- src/extensions/magic_mount.rs
+++ src/extensions/magic_mount.rs
@@ -1,42 +1,52 @@
 mod chunk;
 
-use crate::colink_proto::*;
-
 type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 
 impl crate::application::CoLink {
-    async fn _mm_create_entry(&self, key_name: &str, payload: &[u8]) -> Result<String, Error> {
-        let token = key_name.split("$").last().unwrap();
+    pub(crate) async fn _mm_create_entry(
+        &self,
+        key_name: &str,
+        payload: &[u8],
+    ) -> Result<String, Error> {
+        let split_key = key_name.split("$").collect::<Vec<&str>>();
+        let token = split_key[split_key.len() - 1];
+        let key_name = split_key[0];
         if token.eq("chunk") {
-            let response = self._create_entry_chunk(key_name, payload).await?;
-            return Ok(response);
+            return self._create_entry_chunk(key_name, payload).await;
         }
         return Err("invalid storage option".into());
     }
 
-    async fn _mm_read_entry(&self, key_name: &str) -> Result<Vec<u8>, Error> {
-        let token = key_name.split("$").last().unwrap();
+    pub(crate) async fn _mm_read_entry(&self, key_name: &str) -> Result<Vec<u8>, Error> {
+        let split_key = key_name.split("$").collect::<Vec<&str>>();
+        let token = split_key[split_key.len() - 1];
+        let key_name = split_key[0];
         if token.eq("chunk") {
-            let response = self._read_entry_chunk(key_name).await?;
-            return Ok(response);
+            return self._read_entry_chunk(key_name).await;
         }
         return Err("invalid storage option".into());
     }
 
-    async fn _mm_update_entry(&self, key_name: &str, payload: &[u8]) -> Result<(), Error> {
-        let token = key_name.split("$").last().unwrap();
+    pub(crate) async fn _mm_update_entry(
+        &self,
+        key_name: &str,
+        payload: &[u8],
+    ) -> Result<String, Error> {
+        let split_key = key_name.split("$").collect::<Vec<&str>>();
+        let token = split_key[split_key.len() - 1];
+        let key_name = split_key[0];
         if token.eq("chunk") {
-            let response = self._update_entry_chunk(key_name, payload).await?;
-            return Ok(response);
+            return self._update_entry_chunk(key_name, payload).await;
         }
         return Err("invalid storage option".into());
     }
 
-    async fn _mm_delete_entry(&self, key_name: &str) -> Result<(), Error> {
-        let token = key_name.split("$").last().unwrap();
+    pub(crate) async fn _mm_delete_entry(&self, key_name: &str) -> Result<String, Error> {
+        let split_key = key_name.split("$").collect::<Vec<&str>>();
+        let token = split_key[split_key.len() - 1];
+        let key_name = split_key[0];
         if token.eq("chunk") {
-            let response = self._delete_entry_chunk(key_name).await?;
-            return Ok(response);
+            return self._delete_entry_chunk(key_name).await;
         }
         return Err("invalid storage option".into());
     }
--- src/extensions/magic_mount/chunk.rs
+++ src/extensions/magic_mount/chunk.rs
@@ -1,18 +1,23 @@
-use crate::application::{generate_request, CoLinkClient};
 use crate::colink_proto::*;
+use async_recursion::async_recursion;
 
 const CHUNK_SIZE: usize = 1 * 1024 * 1024; // use 1MB chunks
 
 type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 
 impl crate::application::CoLink {
-    async fn _create_entry_chunk(&self, key_name: &str, payload: &[u8]) -> Result<String, Error> {
-        let metadata_key = format!("{}::metadata", key_name);
+    #[async_recursion]
+    pub(crate) async fn _create_entry_chunk(
+        &self,
+        key_name: &str,
+        payload: &[u8],
+    ) -> Result<String, Error> {
+        let metadata_key = format!("{}:chunk_metadata", key_name);
 
         // lock the metadata entry to prevent simultaneous writes
         self.create_entry(
-            metadata_key.clone(),
-            "creation-in-progress-locked".into_bytes(),
+            &metadata_key.clone(),
+            &("creation-in-progress-locked".to_string().into_bytes()),
         )
         .await?;
 
@@ -28,31 +33,30 @@ impl crate::application::CoLink {
             };
             let chunk = payload[offset..offset + chunk_size].to_vec();
             let response = self
-                .create_entry(format!("{}::{}", key_name, chunk_id), chunk)
+                .create_entry(&format!("{}{}", key_name, chunk_id), &chunk)
                 .await?;
-            chunk_paths.push(response.get_ref().key_path.clone());
+            chunk_paths.push(response);
             offset += chunk_size;
             chunk_id += 1;
         }
 
         // store the chunk paths in the metadata entry and update metadata
-        let response = self
-            .update_entry(metadata_key.clone(), chunk_paths.join(";").into_bytes())
-            .await?;
-        Ok(response.get_ref().key_path.clone())
+        return self
+            .update_entry(&metadata_key.clone(), &chunk_paths.join(";").into_bytes())
+            .await;
     }
 
-    async fn _read_entry_chunk(&self, key_name: &str) -> Result<Vec<u8>, Error> {
-        let metadata_key = format!("{}::metadata", key_name);
+    pub(crate) async fn _read_entry_chunk(&self, key_name: &str) -> Result<Vec<u8>, Error> {
+        let metadata_key = format!("{}:chunk_metadata", key_name);
         let metadata_response = self
-            .read_entries(vec![StorageEntry {
-                key_path: metadata_key,
+            .read_entries(&vec![StorageEntry {
+                key_name: metadata_key.clone(),
                 ..Default::default()
             }])
             .await?;
 
         // check if the metadata is locked
-        let payload_string = String::from_utf8(response.get_ref().entries[0].payload.clone())?;
+        let payload_string = String::from_utf8(metadata_response[0].payload.clone())?;
         if payload_string.ends_with("locked") {
             return Err("Creation in progress".into());
         }
@@ -62,31 +66,27 @@ impl crate::application::CoLink {
         let mut payload = Vec::new();
         for chunk in chunks {
             let response = self
-                .read_entries(vec![StorageEntry {
+                .read_entries(&vec![StorageEntry {
                     key_path: chunk.to_string(),
                     ..Default::default()
                 }])
                 .await?;
-            payload.extend(response.get_ref().entries[0].payload.clone());
+            payload.push(response[0].key_path.clone());
         }
-
-        // parse payload into storage entry
-        let storage_entry = StorageEntries {
-            entries: vec![StorageEntry {
-                key_path: metadata_response.get_ref().entries[0].key_path.clone(),
-                payload,
-                ..Default::default()
-            }],
-        };
-        Ok(storage_entry)
+        Ok(payload.join(";").into_bytes())
     }
 
-    async fn _update_entry_chunk(&self, key_name: &str, payload: &[u8]) -> Result<String, Error> {
-        let metadata_key = format!("{}::metadata", key_name);
+    #[async_recursion]
+    pub(crate) async fn _update_entry_chunk(
+        &self,
+        key_name: &str,
+        payload: &[u8],
+    ) -> Result<String, Error> {
+        let metadata_key = format!("{}:chunk_metadata", key_name);
 
         // check if the metadata is locked
-        let response = self.read_entry(metadata_key.clone()).await?;
-        let payload_string = String::from_utf8(response.get_ref().payload.clone())?;
+        let response = self.read_entry(&metadata_key.clone()).await?;
+        let payload_string = String::from_utf8(response.clone())?;
         if payload_string.ends_with("locked") {
             return Err("Creation in progress".into());
         }
@@ -103,27 +103,24 @@ impl crate::application::CoLink {
             };
             let response = self
                 .update_entry(
-                    format!("{}::{}", key_name, chunk_id),
-                    payload[offset..offset + chunk_size].to_vec(),
+                    &format!("{}:{}", key_name, chunk_id),
+                    &payload[offset..offset + chunk_size].to_vec(),
                 )
                 .await?;
-
-            chunk_paths.push(response.get_ref().key_path.clone());
-
+            chunk_paths.push(response);
             offset += chunk_size;
             chunk_id += 1;
         }
 
         // update metadata with new chunk paths
-        let response = self
-            .update_entry(metadata_key.clone(), chunk_paths.join(";").into_bytes())
-            .await?;
-        Ok(response.get_ref().key_path.clone())
+        return self
+            .update_entry(&metadata_key.clone(), &chunk_paths.join(";").into_bytes())
+            .await;
     }
 
-    async fn _delete_entry_chunk(&self, key_name: &str) -> Result<(), Error> {
-        let metadata_key = format!("{}::metadata", key_name);
-        let response = self.delete_entry(metadata_key.clone()).await?;
-        return Ok(response.get_ref().key_path.clone());
+    #[async_recursion]
+    pub(crate) async fn _delete_entry_chunk(&self, key_name: &str) -> Result<String, Error> {
+        let metadata_key = format!("{}:chunk_metadata", key_name);
+        return self.delete_entry(&metadata_key.clone()).await;
     }
 }
