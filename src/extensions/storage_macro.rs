392d5393de558d46ecba554d8a4af4d4a1c0d9c9
nociza <azicon@berkeley.edu>
2022-11-25 06:12:57 -0800

    Generalize macro_type parsing

--- src/extensions/storage_macro.rs
+++ src/extensions/storage_macro.rs
@@ -3,28 +3,56 @@ mod chunk;
 type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 
 impl crate::application::CoLink {
+    fn _parse_macro(&self, key_name: &str) -> (String, String, String) {
+        let split_key = key_name.split(':').collect::<Vec<&str>>();
+        let mut macro_type = String::new();
+        for s in split_key.iter().rev() {
+            if s.contains('$') {
+                macro_type = s.replace("$", "");
+                break;
+            }
+        }
+        let split_by_macro = key_name
+            .split(&format!(":${}:", macro_type))
+            .collect::<Vec<&str>>();
+
+        (
+            split_by_macro[0].to_string(),
+            macro_type,
+            split_by_macro[1].to_string(),
+        )
+    }
+
     pub(crate) async fn _sm_create_entry(
         &self,
         key_name: &str,
         payload: &[u8],
     ) -> Result<String, Error> {
-        let split_key = key_name.split('$').collect::<Vec<&str>>();
-        let token = split_key[split_key.len() - 1];
-        let key_name = split_key[0];
-        if token.eq("chunk") {
-            return self._create_entry_chunk(key_name, payload).await;
+        let parsed_tuple = self._parse_macro(key_name);
+        let macro_type = parsed_tuple.1.as_str();
+        let string_before = parsed_tuple.0.as_str();
+        match macro_type {
+            "chunk" => self._create_entry_chunk(string_before, payload).await,
+            _ => Err(format!(
+                "invalid storage option, found {} in key name {}",
+                macro_type, key_name
+            )
+            .into()),
         }
-        Err("invalid storage option".into())
     }
 
     pub(crate) async fn _sm_read_entry(&self, key_name: &str) -> Result<Vec<u8>, Error> {
-        let split_key = key_name.split('$').collect::<Vec<&str>>();
-        let token = split_key[split_key.len() - 1];
-        let key_name = split_key[0];
-        if token.eq("chunk") {
-            return self._read_entry_chunk(key_name).await;
+        let parsed_tuple = self._parse_macro(key_name);
+        let macro_type = parsed_tuple.1.as_str();
+        let string_before = parsed_tuple.0.as_str();
+        match macro_type {
+            "chunk" => self._read_entry_chunk(string_before).await,
+            _ => Err(format!(
+                "invalid storage option, found {} in key name {}",
+                macro_type, key_name
+            )
+            .into()),
         }
-        Err("invalid storage option".into())
     }
 
     pub(crate) async fn _sm_update_entry(
@@ -32,22 +60,30 @@ impl crate::application::CoLink {
         key_name: &str,
         payload: &[u8],
     ) -> Result<String, Error> {
-        let split_key = key_name.split('$').collect::<Vec<&str>>();
-        let token = split_key[split_key.len() - 1];
-        let key_name = split_key[0];
-        if token.eq("chunk") {
-            return self._update_entry_chunk(key_name, payload).await;
+        let parsed_tuple = self._parse_macro(key_name);
+        let macro_type = parsed_tuple.1.as_str();
+        let string_before = parsed_tuple.0.as_str();
+        match macro_type {
+            "chunk" => self._update_entry_chunk(string_before, payload).await,
+            _ => Err(format!(
+                "invalid storage option, found {} in key name {}",
+                macro_type, key_name
+            )
+            .into()),
         }
-        Err("invalid storage option".into())
     }
 
     pub(crate) async fn _sm_delete_entry(&self, key_name: &str) -> Result<String, Error> {
-        let split_key = key_name.split('$').collect::<Vec<&str>>();
-        let token = split_key[split_key.len() - 1];
-        let key_name = split_key[0];
-        if token.eq("chunk") {
-            return self._delete_entry_chunk(key_name).await;
+        let parsed_tuple = self._parse_macro(key_name);
+        let macro_type = parsed_tuple.1.as_str();
+        let string_before = parsed_tuple.0.as_str();
+        match macro_type {
+            "chunk" => self._delete_entry_chunk(string_before).await,
+            _ => Err(format!(
+                "invalid storage option, found {} in key name {}",
+                macro_type, key_name
+            )
+            .into()),
         }
-        Err("invalid storage option".into())
     }
 }
