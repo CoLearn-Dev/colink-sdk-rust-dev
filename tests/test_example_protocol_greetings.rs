69ee8fd5dd4cab7bcd20050de916bb94f82f65a3
stneng <git@stneng.com>
2023-01-03 09:48:14 +0000

    update test

--- tests/test_example_protocol_greetings.rs
+++ tests/test_example_protocol_greetings.rs
@@ -3,12 +3,14 @@ use colink::{
     Participant,
 };
 use rand::Rng;
+use std::process::{Child, Command, Stdio};
 
 const USER_NUM: [usize; 11] = [2, 2, 2, 2, 2, 3, 3, 4, 4, 5, 5];
 
 #[tokio::test]
 async fn test_greetings() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
     tracing_subscriber::fmt::init();
+    build();
     for i in 0..11 {
         test_main(USER_NUM[i as usize]).await?;
     }
@@ -21,28 +23,25 @@ async fn test_main(
     let _ir = InstantRegistry::new().await;
     let mut iss = vec![];
     let mut cls = vec![];
+    let mut users = vec![];
     for _ in 0..user_num {
         let is = InstantServer::new();
         let cl = is.get_colink().switch_to_generated_user().await?;
+        let user = cl.generate_token("user").await?;
         iss.push(is);
         cls.push(cl);
+        users.push(user);
     }
     for i in 0..user_num {
         let num: usize = rand::thread_rng().gen_range(1..4); // Generate the number of operators for testing multiple protocol operators.
         for _ in 0..num {
             {
-                let cl = cls[i].clone();
                 let time: u64 = rand::thread_rng().gen_range(0..1000);
+                let addr = cls[i].get_core_addr()?;
+                let user = users[i].clone();
                 std::thread::spawn(move || {
                     std::thread::sleep(core::time::Duration::from_millis(time));
-                    tokio::runtime::Builder::new_multi_thread()
-                        .enable_all()
-                        .build()
-                        .unwrap()
-                        .block_on(async move {
-                            cl.start_protocol_operator("greetings", &cl.get_user_id()?, false)
-                                .await
-                        })
+                    run_protocol_greetings(&addr, &user)
                 })
             };
         }
@@ -72,4 +71,34 @@ async fn test_main(
         assert!(msg.parse::<i32>()? == random_number);
     }
     Ok(())
+}
+
+fn build() {
+    let mut sdk = Command::new("cargo")
+        .args(["build", "--all-targets"])
+        .current_dir("./")
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .spawn()
+        .unwrap();
+    sdk.wait().unwrap();
+}
+
+fn run_protocol_greetings(addr: &str, jwt: &str) -> Child {
+    Command::new("cargo")
+        .args([
+            "run",
+            "--example",
+            "protocol_greetings",
+            "--",
+            "--addr",
+            addr,
+            "--jwt",
+            jwt,
+        ])
+        .current_dir("./")
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .spawn()
+        .unwrap()
 }
