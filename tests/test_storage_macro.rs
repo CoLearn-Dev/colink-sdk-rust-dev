0960af2ba51d49c702781983fea9d07a3ba805db
stneng <git@stneng.com>
2023-01-17 11:15:54 +0800

    add test

---
+++ tests/test_storage_macro.rs
@@ -0,0 +1,49 @@
+use colink::{
+    extensions::instant_server::{InstantRegistry, InstantServer},
+    CoLink,
+};
+use rand::Rng;
+
+#[tokio::test]
+async fn test_storage_macro() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+    let _ir = InstantRegistry::new();
+    let is = InstantServer::new();
+    let cl = is.get_colink().switch_to_generated_user().await?;
+
+    // chunk
+    let key_name = "storage_macro_test_chunk:$chunk";
+    test_crud(&cl, key_name).await?;
+
+    // redis
+    cl.create_entry("storage_macro_test_redis:redis_url", b"redis://localhost")
+        .await?;
+    let key_name = "storage_macro_test_redis:$redis:redis_key";
+    test_crud(&cl, key_name).await?;
+
+    Ok(())
+}
+
+async fn test_crud(
+    cl: &CoLink,
+    key_name: &str,
+) -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+    let payload = rand::thread_rng()
+        .sample_iter(&rand::distributions::Standard)
+        .take(5e6 as usize)
+        .collect::<Vec<u8>>();
+    cl.create_entry(key_name, &payload.clone()).await?;
+    assert!(cl.create_entry(key_name, b"").await.is_err());
+    let data = cl.read_entry(key_name).await?;
+    assert_eq!(data, payload);
+    let new_payload = rand::thread_rng()
+        .sample_iter(&rand::distributions::Standard)
+        .take(3e6 as usize)
+        .collect::<Vec<u8>>();
+    cl.update_entry(key_name, &new_payload.clone()).await?;
+    let data = cl.read_entry(key_name).await?;
+    assert_eq!(data, new_payload);
+    cl.delete_entry(key_name).await?;
+    assert!(cl.read_entry(key_name).await.is_err());
+    assert!(cl.delete_entry(key_name).await.is_err());
+    Ok(())
+}
