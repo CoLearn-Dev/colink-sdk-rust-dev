1b5f969907050bde563c2d87ad461812fcf5f558
stneng <git@stneng.com>
2023-01-26 12:54:51 +0000

    add test_storage_macro_append

--- tests/test_storage_macro.rs
+++ tests/test_storage_macro.rs
@@ -32,6 +32,31 @@ async fn test_storage_macro_redis() -> Result<(), Box<dyn std::error::Error + Se
     Ok(())
 }
 
+#[tokio::test]
+async fn test_storage_macro_append(
+) -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+    let _ir = InstantRegistry::new();
+    let is = InstantServer::new();
+    let cl = is.get_colink().switch_to_generated_user().await?;
+
+    let key_name = "storage_macro_test_append";
+    let payload0 = rand::thread_rng()
+        .sample_iter(&rand::distributions::Standard)
+        .take(10)
+        .collect::<Vec<u8>>();
+    let payload1 = rand::thread_rng()
+        .sample_iter(&rand::distributions::Standard)
+        .take(10)
+        .collect::<Vec<u8>>();
+    cl.create_entry(key_name, &payload0).await?;
+    cl.update_entry(&format!("{}:$append", key_name), &payload1)
+        .await?;
+    let data = cl.read_entry(key_name).await?;
+    assert_eq!(data, [payload0, payload1].concat());
+
+    Ok(())
+}
+
 #[ignore]
 #[tokio::test]
 async fn test_storage_macro_chunk_redis(
@@ -59,7 +84,7 @@ async fn test_crud(
         .sample_iter(&rand::distributions::Standard)
         .take(5e6 as usize)
         .collect::<Vec<u8>>();
-    cl.create_entry(key_name, &payload.clone()).await?;
+    cl.create_entry(key_name, &payload).await?;
     assert!(cl.create_entry(key_name, b"").await.is_err());
     let data = cl.read_entry(key_name).await?;
     assert_eq!(data, payload);
@@ -67,7 +92,7 @@ async fn test_crud(
         .sample_iter(&rand::distributions::Standard)
         .take(3e6 as usize)
         .collect::<Vec<u8>>();
-    cl.update_entry(key_name, &new_payload.clone()).await?;
+    cl.update_entry(key_name, &new_payload).await?;
     let data = cl.read_entry(key_name).await?;
     assert_eq!(data, new_payload);
     cl.delete_entry(key_name).await?;
