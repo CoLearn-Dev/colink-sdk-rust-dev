bd74063971756d40b9fe956636a821e9c37eddbc
stneng <git@stneng.com>
2023-02-13 01:45:41 +0000

    fix

--- tests/download-server.sh
+++ tests/download-server.sh
@@ -2,7 +2,11 @@
 set -e
 rm -rf colink-server
 mkdir colink-server && cd colink-server
-wget https://github.com/CoLearn-Dev/colink-server-dev/releases/download/v0.3.2/colink-server-linux-x86_64.tar.gz
-tar -xzf colink-server-linux-x86_64.tar.gz
+PACKAGE_NAME="colink-server-linux-x86_64.tar.gz"
+if [ "$(uname)" == "Darwin" ]; then
+    PACKAGE_NAME="colink-server-macos-x86_64.tar.gz"
+fi
+wget https://github.com/CoLearn-Dev/colink-server-dev/releases/download/v0.3.2/$PACKAGE_NAME
+tar -xzf $PACKAGE_NAME
 touch user_init_config.toml # create an empty user init config to prevent automatically starting protocols when importing users.
 cd ..
